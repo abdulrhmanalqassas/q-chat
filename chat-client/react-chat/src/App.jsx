fdc475a9849c790f1a09c313aa23f0f3f96eac81
abdulrhmanalqassas <abdulrhmanalqassas@gmail.com>
2024-08-21 15:33:32 -0700

    feat: Implement POST method for /test endpoint
    - Update backend route from GET to POST
    - Adjust frontend to send prompt in request body
    - Display sanitized response in HTML article

--- chat-client/react-chat/src/App.jsx
+++ chat-client/react-chat/src/App.jsx
@@ -1,46 +1,83 @@
-import { useState  ,useEffect  } from 'react'
+import { useState  ,useEffect ,useRef } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import DOMPurify from 'dompurify'
+import { marked } from 'marked'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
   const [serverData, setServerData] = useState('')
+  const [userPrompt, setUserPrompt] = useState(null)
+  const inputRef = useRef(null)
 
   useEffect (() => {
-    fetch("/test")
-    .then((res) => { return res.json()})
-    .then(
-      (data)=>{
-        setServerData(data.data)
-        console.log(data)
-      }
-    )
+
+    fetch("/test",{
+      method:"POST",
+      headers:{
+        "Content-Type":"application/json",
+      },
+      body:JSON.stringify({"prompt":"hi "})
+    }) 
+    .then((res)=>res.json()  ) .then(
+  (data)=>{
+    const html = DOMPurify.sanitize(marked(data.data))
+    setServerData({ data:data.data, html })
+      setUserPrompt('')
+   
+  }
+)
+
     }
     ,[])
+
+
+    function handleKeyDown(e) {
+      if (e.key === 'Enter' && !e.shiftKey) {
+        e.preventDefault();
+        inputRef.current.blur()
+        handleSubmit()
+      }
+    }
+   function handleSubmit(){
+   
+    fetch("/test",{
+      method:"POST",
+      headers:{
+        "Content-Type":"application/json",
+      },
+      body:JSON.stringify({"prompt":userPrompt})
+    }) 
+    .then((res)=>res.json()  ) .then(
+  (data)=>{
+    const html = DOMPurify.sanitize(marked(data.data))
+    setServerData({ data:data.data, html })
+    inputRef.current.focus()
+    setUserPrompt('')
+   
+  }
+)
+   } 
+    
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+
+      <main style={{width: '90vw', height: '100vh', display: 'flex', flexDirection: 'column' }}>
+      <h1 style={{ padding: '10px', marginBottom: '0' }}>MyPrompter</h1>
+      <div style={{ margin: '0', flexGrow: '1', overflow: 'scroll' }}>
+        <div style={{ width: '100%', height: '100%' }}>
+          {
+            (serverData === '') ? ('Loading...') :
+              <article style={{ margin: '0' }} dangerouslySetInnerHTML={{ __html: serverData.html }} />
+          }
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          {serverData}
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div style={{ display: 'flex', alignItems: 'end', backgroundColor: '#222', padding: '10px' }}>
+        <textarea onChange={(e) => setUserPrompt(e.target.value)} onKeyDown={handleKeyDown} ref={inputRef} value={userPrompt ? userPrompt : ""} style={{ margin: '0', flexGrow: '1', overflowY: 'hidden' }} placeholder='Type in Prompt...' />
+        <button onClick={handleSubmit} style={{ margin: '0', flex: '1', marginLeft: '10px' }}>Go</button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    </main>
     </>
   )
 }
--- gemini-server/index.js
+++ gemini-server/index.js
@@ -1,32 +1,23 @@
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 const dotenv = require('dotenv');
 const express = require('express');
-const app = express()
+const bodyParser = require('body-parser');
+const app = express() ;
+app.use(bodyParser.json());
+
 
 dotenv.config();
 // Access your API key as an environment variable 
 const genAI = new GoogleGenerativeAI(process.env.API_KEY);
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash"});
 
-const CVprompt = "Write a cv fore frontend dev caled abdulrhman use html notation and in line css with cool colerd background"
+const CVprompt = "Write a wibsite  for a teatcher  caled eng.amr elsi3idy use html notation and in line css with cool colerd background and swibe images with studint"
 const testPrompt = "Write a  childern book   "
 const startPrompt = "Write a chat bot server home bage and  use html notation and in line css with cool gradint anmated colord background"
 
 const resultMess = null ; 
 
-// app.get("./api",(req,res)=>{
 
-//      resultMess = response.text();
-//      model.generateContent(prompt).then(()=>{
-//         const response =  result.response;
-//         console.log("MMMMMMMMMMMMMMMM<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>",response);
-//         const text = response.text();
-//         resultMess = response
-//         console.log(text);
-//         res.send(resultMess)
-//      });
-
-// })
 app.get('/api', (req, res) => {
  
   model.generateContent(CVprompt).then((result)=>{
@@ -35,13 +26,13 @@ app.get('/api', (req, res) => {
             const text = response.text();
          
             console.log(text);
-            res.send({"data":text})
+            res.send(text)
          });
 });
 
-app.get('/test', (req, res) => {
- 
-  model.generateContent(testPrompt).then((result)=>{
+app.post('/test', (req, res) => {
+ const prompt = req.body.prompt
+  model.generateContent(prompt).then((result)=>{
             const response =  result.response;
             console.log("0000000000000000000000000000000000000000000000000000000000000xxxxxxxxxxxxxxxxxxxx",response);
             const text = response.text();
